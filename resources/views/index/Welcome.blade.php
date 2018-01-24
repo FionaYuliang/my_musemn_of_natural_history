3999d3e4496aa390f7b0ca6c1df2a4b7bd9c8329
yuliang <yangmingming94@gmail.com>
2018-01-25 02:02:39 +0800

    测试push功能

--- resources/views/index/Welcome.blade.php
+++ resources/views/index/Welcome.blade.php
@@ -121,7 +121,6 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
